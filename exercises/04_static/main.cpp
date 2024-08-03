e2dee8480446f49a2ba42256de6c9014b3d532b2
Wang Yongqi <shuwyq@foxmail.com>
2024-08-03 13:13:31 +0800

    fix bug

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -2,18 +2,20 @@
 
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
+// 第一个static是表示该函数是静态函数，
+// 第二个static是表示该变量是静态变量，可以在程序中一直存在,只会进行一次初始化，后续不再进行初始化。
 static int func(int param) {
-    static int static_ = param;
-    // std::cout << "static_ = " << static_ << std::endl;
+    static int static_ = param; //只会在第一次运行时运行，后续直接跳过
+    std::cout << "static_ = " << static_ << std::endl;
     return static_++;
 }
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }
--- exercises/13_class_clone/main.cpp
+++ exercises/13_class_clone/main.cpp
@@ -7,20 +7,32 @@
 class DynFibonacci {
     size_t *cache;
     int cached;
+    int capacity;
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0),capacity(capacity) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const & old){
+        size_t length = old.capacity;
+        cache = new size_t[length];
+        for(int i = 0;i < length; ++i){
+            cache[i] = old.cache[i];
+        }
+        cached = old.cached;
+        capacity = length;
+    };
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() { delete[] cache; };
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (cached=2; cached<=i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
