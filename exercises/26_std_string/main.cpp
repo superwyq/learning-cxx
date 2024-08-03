c2c4acc5f201775d05d4a8661da7fc9523a1bda8
Wang Yongqi <shuwyq@foxmail.com>
2024-08-03 20:40:22 +0800

    finish all

--- exercises/26_std_string/main.cpp
+++ exercises/26_std_string/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include "../exercise.h"
 #include <string>
+#include <type_traits>
 
 // READ: 字符串 <https://zh.cppreference.com/w/cpp/string/basic_string>
 
@@ -7,12 +8,12 @@ int main(int argc, char **argv) {
     // READ: 字符串字面量 <https://zh.cppreference.com/w/cpp/string/basic_string/operator%22%22s>
     using namespace std::string_literals;
     auto hello = "Hello"s;
-    auto world = "world";
+    auto world = "world"; //字符串字面量，编译器会将其单独存储在一段内存中，所以world类型是const char*
     // READ: `decltype` 表达式 <https://zh.cppreference.com/w/cpp/language/decltype>
     // READ: `std::is_same_v` 元编程判别 <https://zh.cppreference.com/w/cpp/types/is_same>
-    ASSERT((std::is_same_v<decltype(hello), ?>), "Fill in the missing type.");
-    ASSERT((std::is_same_v<decltype(world), ?>), "Fill in the missing type.");
+    ASSERT((std::is_same_v<decltype(hello), std::string>), "Fill in the missing type.");
+    ASSERT((std::is_same_v<decltype(world), const char *>), "Fill in the missing type.");
     // TODO: 将 `?` 替换为正确的字符串
-    ASSERT(hello + ", " + world + '!' == "?", "Fill in the missing string.");
+    ASSERT(hello + ", " + world + '!' == "Hello, world!", "Fill in the missing string.");
     return 0;
 }
